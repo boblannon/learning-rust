def6e6cccad11bb88608ef0a7821927e90ffc489
Bob Lannon <bob.lannon@mapbox.com>
2018-04-06 16:20:09 -0400

    notes

--- the-book/01/02_hello-world/main.rs
+++ the-book/01/02_hello-world/main.rs
@@ -1,3 +1,11 @@
+// define a function, no params, no return
 fn main() {
+    // println is a macro
     println!("Hello, world!");
+    // why?
+    // - string arg can have formatting specifiers
+    //      - checked at compile-time
+    //      - can have named args (rust fcts can't)
+    // - can take variable number of args
+    // - implicitly takes args by ref (even when passed by value)
 }
