6f793d2ab3e0e4b21ab50b5ab4ba9c32af0d5dcf
Bob Lannon <bob.lannon@mapbox.com>
2018-04-12 11:49:11 -0700

    debug output

--- the-book/05_structs/rectangles/src/main.rs
+++ the-book/05_structs/rectangles/src/main.rs
@@ -1,3 +1,5 @@
+// you can get debug output, but you have to opt in
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -5,6 +7,15 @@ struct Rectangle {
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+
+    // requires std::fmt::Display trait on Rectangle
+    //println!("rect1 is {}", rect1);
+
+    // basic debug printout, but must opt in with #[derive(Debug)]
+    println!("rect1 is {:?}", rect1);
+
+    // pretty-printed debug output, with newlines and indentation
+    println!("rect1 is {:#?}", rect1);
 
     println!(
         "The area of the rect is {} square pixels",
