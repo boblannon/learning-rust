2dfc2d2bb1f35e3be50c6fcea32fdf0bd42f9210
Bob Lannon <bob.lannon@mapbox.com>
2018-04-16 09:55:55 -0700

    recoverable errors

---
+++ the-book/09_error-handling/recoverable/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        // ref keeps the variable from being moved into the arm (see ch18 for
+        // why it's not &
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        "Tried to create file but there was a problem: {:?}",
+                        e
+                    )
+                },
+            }
+        },
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
+}
