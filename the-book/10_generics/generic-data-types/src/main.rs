55332476a6848e8521dda8802e0c0471344700d5
Bob Lannon <bob.lannon@mapbox.com>
2018-04-16 11:55:10 -0700

    generics in structs

--- the-book/10_generics/generic-data-types/src/main.rs
+++ the-book/10_generics/generic-data-types/src/main.rs
@@ -10,6 +10,7 @@ fn largest_i32(list: &[i32]) -> i32 {
     largest
 }
 
+// this function only differs in param and return type. internal code is identical
 fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
 
@@ -22,6 +23,32 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
+// generic T
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         // this errors at first because not all types can by used with the > operation. that's
+//         // because not all types have an implementation of the trait std::cmp::PartialOrd
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// require that two fields in a struct be of the same type
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+// allow type of fields to be different
+struct PointTwo<T, U> {
+    x: T,
+    y: U,
+}
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
@@ -33,5 +60,16 @@ fn main() {
 
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+
+    let both_integer = Point{ x: 5, y: 10 };
+    let both_float = Point{ x: 1.0, y: 4.0 };
+    // This won't work, since int and float
+    // let integer_and_float = Point { x: 5, y: 4.0 }; // err: mismatched types
+
+    // these will all work (okay if T and U happen to be the same type
+    let both_integer = PointTwo { x: 5, y: 10 };
+    let both_float = PointTwo { x: 1.0, y: 4.0 };
+    let integer_and_float = PointTwo { x: 5, y: 4.0 };
+
 }
 
