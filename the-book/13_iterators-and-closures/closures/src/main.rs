af0b1dff6f8a625b2551b79bc4b0f2dc1ec7a6f4
Bob Lannon <bob.lannon@mapbox.com>
2018-04-17 15:44:21 -0700

    memoization using custom struct

--- the-book/13_iterators-and-closures/closures/src/main.rs
+++ the-book/13_iterators-and-closures/closures/src/main.rs
@@ -1,27 +1,65 @@
 use std::thread;
 use std::time::Duration;
 
-// closures can look similar to functions, but don't have to:
+//// closures can look similar to functions, but don't have to:
 // fn  add_one_v1   (x: u32) -> u32 { x + 1 }
 // let add_one_v2 = |x: u32| -> u32 { x + 1 };
 // let add_one_v3 = |x|             { x + 1 };
 // let add_one_v4 = |x|               x + 1  ;
 
+//// closure type inference is done once for the first time called.
+// let example_closure = |x| x;
+//
+// let s = example_closure(String::from("hello"));
+// let n = example_closure(5); // error: mismatched types
+
+//// Memoization (or Lazy Evaluation)
+// every closure instance has its own unique anonymous type, so we need to use a generic T. All
+// closures implement one of the Fn traits, though, so we can set a trait bound.
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -29,7 +67,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
@@ -38,6 +76,20 @@ fn generate_workout(intensity: u32, random_number: u32) {
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+
+    let simulated_user_specified_value = 26;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+
+    let simulated_random_number = 3;
 
     generate_workout(
         simulated_user_specified_value,
